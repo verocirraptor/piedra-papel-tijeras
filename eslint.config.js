bb09a4b07e4fb24185b23add9d2c62bc5027e42f
Veronica Giraldo Garrido <veronica.giraldo.garrido@gmail.com>
2025-04-02 17:43:10 +0200

    Juego de piedra-papel-tijeras en JavaScript.

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import { recommended } from "eslint-config-manzdev";
+
+export default [
+  ...recommended,
+  {
+    rules: {
+      /* Tus reglas aquí */
+	 }
+  }
+];
---
+++ src/index.js
@@ -0,0 +1,12 @@
+const jugador1 = prompt("Escoge entre: Piedra / Papel / Tijeras");
+const jugador2 = prompt("Escoge entre: Piedra / Papel / Tijeras");
+
+if ((jugador1 === "Piedra" && jugador2 === "Piedra") || (jugador1 === "Papel" && jugador2 === "Papel") || (jugador1 === "Tijeras" && jugador2 === "Tijeras")) {
+  alert("¡Empate!");
+} else if ((jugador1 === "Piedra" && jugador2 === "Papel") || (jugador1 === "Papel" && jugador2 === "Tijeras") || (jugador1 === "Tijeras" && jugador2 === "Piedra")) {
+  alert("¡Gana jugador2!");
+} else if ((jugador1 === "Piedra" && jugador2 === "Tijeras") || (jugador1 === "Papel" && jugador2 === "Piedra") || (jugador1 === "Tijeras" && jugador2 === "Papel")) {
+  alert("¡Gana jugador1!");
+} else {
+  alert("¡Error! ¡Inténtalo de nuevo!");
+}
